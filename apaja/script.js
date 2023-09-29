af0eed175bbf55941a243a882e2905a60f928aae
assan07 <hasanwkl04@gmail.com>
2023-09-29 09:10:52 +0800

    Learn Web Program 1

---
+++ apaja/script.js
@@ -0,0 +1,6 @@
+function calculateArea(){
+const base = 100; //panjang alas segitiga dalam paksel
+const height = 87; // tinggi segitiga dalam piksel
+const area = 0.5 * base * height; // rumus luas segitiga
+document.getElementById("result").innerHTML= "Luas segitiga adalah : "+ area + "piksel persegi";
+}
